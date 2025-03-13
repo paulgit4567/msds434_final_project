2777b5acd00b43ae97862c4286fe59cf56f276e9
Paul Yaginuma <paul@Pauls-MacBook-Air.local>
2025-03-13 18:29:27 +0900

    update

--- code/filter_for_pga_sg_and_stats.py
+++ code/filter_for_pga_sg_and_stats.py
@@ -5,7 +5,7 @@ def filter_event_data(event_df):
     event_df["tour"] = event_df["tour"].str.lower()
     event_df["sg_categories"] = event_df["sg_categories"].str.lower()
     event_df["traditional_stats"] = event_df["traditional_stats"].str.lower()
-
+a
     event_df = event_df[
         (event_df["tour"] == "pga") &
         (event_df["sg_categories"] == "yes") &
