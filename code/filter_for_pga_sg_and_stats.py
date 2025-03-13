9dcec785e639bc95dafd3e6d592c8cf2aa3e58fb
paulgit4567 <paulyaginuma@gmail.com>
2025-03-13 18:09:33 +0900

    Add files via upload

---
+++ code/filter_for_pga_sg_and_stats.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def filter_event_data(event_df):
+
+    event_df["tour"] = event_df["tour"].str.lower()
+    event_df["sg_categories"] = event_df["sg_categories"].str.lower()
+    event_df["traditional_stats"] = event_df["traditional_stats"].str.lower()
+
+    event_df = event_df[
+        (event_df["tour"] == "pga") &
+        (event_df["sg_categories"] == "yes") &
+        (event_df["traditional_stats"] == "yes")
+    ]
+    return event_df
