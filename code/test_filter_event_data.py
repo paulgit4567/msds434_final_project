19e414756ba34a34c379c56312c4b2c985f3c9c5
paulgit4567 <paulyaginuma@gmail.com>
2025-03-13 18:42:41 +0900

    Add files via upload

---
+++ code/test_filter_event_data.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from filter_events import filter_event_data
+
+def test_filter_event_data():
+
+    data = {
+        "calendar_year": [2025, 2025],
+        "date": ["2025-02-16", "2025-02-09"],
+        "event_id": [7, 3],
+        "event_name": ["The Genesis Invitational", "Pauls Made Up Tournament"],
+        "sg_categories": ["yes", "no"],
+        "tour": ["pga", "abc"],
+        "traditional_stats": ["yes", "no"]
+    }
+    df = pd.DataFrame(data)
+
+    filtered_df = filter_event_data(df)
+    assert len(filtered_df) == 1
+    assert filtered_df.iloc[0]["event_id"] == 7
+    assert filtered_df.iloc[0]["event_name"] == "The Genesis Invitational"
